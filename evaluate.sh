a5d511bce7cfc02d3cd84d0b3adfa853742406d6
xvnunu <1102712036@qq.com>
2026-06-17 21:49:06 +0800

    update_train&evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,137 @@
+CUDA_VISIBLE_DEVICES=2 \
 python evaluate_stereo.py \
-  --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+  --restore_ckpt checkpoints/defomstereo_vitl_middlebury.pth \
+  --datasets eth3d \
+  --dinov2_encoder vitl \
+  --scale_iters 8 \
+  --idepth_scale 0.5 \
+  --corr_levels 2 \
+  --corr_radius 4 \
+  --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+  --scale_corr_radius 2\
+  --extractor_module extractor_defom
+
+CUDA_VISIBLE_DEVICES=2 \
+python evaluate_stereo.py \
+--restore_ckpt checkpoints/defomstereo_vits_instereo2k_mb2014_mb2021/checkpoint_latest.pth  \
+--datasets eth3d \
+--dinov2_encoder vits \
+--scale_iters 8 \
+--idepth_scale 0.5 \
+--corr_levels 2 \
+--corr_radius 4 \
+--scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+--scale_corr_radius 2
+
+
+
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+#   --datasets eth3d \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2\
+#   --extractor_module extractor_defom
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/defomstereo_vits_middeval3_try1_20000.pth \
+#   --datasets eth3d \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2
+
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+#   --datasets middlebury_Q \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+#   --extractor_module extractor_defom \
+
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/checkpoint_latest.pth \
+#   --datasets middlebury_Q \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+#   --datasets middlebury_H \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+#   --extractor_module extractor_defom \
+
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/checkpoint_latest.pth \
+#   --datasets middlebury_H \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+#   --datasets middlebury_F \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+#   --extractor_module extractor_defom \
+
+
+# CUDA_VISIBLE_DEVICES=2 \
+# python evaluate_stereo.py \
+#   --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/checkpoint_latest.pth \
+#   --datasets middlebury_F \
+#   --dinov2_encoder vits \
+#   --scale_iters 8 \
+#   --idepth_scale 0.5 \
+#   --corr_levels 2 \
+#   --corr_radius 4 \
+#   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+#   --scale_corr_radius 2 \
+
+python evaluate_stereo.py \
+  --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/defomstereo_vits_middeval3_try1_50000.pth \
   --datasets eth3d \
   --dinov2_encoder vits \
   --scale_iters 8 \
@@ -10,7 +142,7 @@ python evaluate_stereo.py \
   --scale_corr_radius 2
 
 python evaluate_stereo.py \
-  --restore_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+  --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/defomstereo_vits_middeval3_try1_60000.pth \
   --datasets eth3d \
   --dinov2_encoder vits \
   --scale_iters 8 \
@@ -18,7 +150,15 @@ python evaluate_stereo.py \
   --corr_levels 2 \
   --corr_radius 4 \
   --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
-  --scale_corr_radius 2 \
-  --extractor_module extractor_defom \
-  --save_vis_dir vis_eth3d \
-  --save_vis_limit 20
+  --scale_corr_radius 2 
+
+python evaluate_stereo.py \
+  --restore_ckpt checkpoints/defomstereo_vits_middeval3_try1/defomstereo_vits_middeval3_try1_70000.pth \
+  --datasets eth3d \
+  --dinov2_encoder vits \
+  --scale_iters 8 \
+  --idepth_scale 0.5 \
+  --corr_levels 2 \
+  --corr_radius 4 \
+  --scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+  --scale_corr_radius 2 
--- train.sh
+++ train.sh
@@ -31,3 +31,31 @@ python -m torch.distributed.launch --nproc_per_node=4 --master_port=9993 train_s
 --image_size 384 512 \
 --resume_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
 2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
+
+
+CHECKPOINT_DIR=checkpoints/defomstereo_vits_instereo2k_mb2014_mb2021
+mkdir -p ${CHECKPOINT_DIR}
+
+nohup env CUDA_VISIBLE_DEVICES=1,2,3 PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128 \
+python -m torch.distributed.launch --nproc_per_node=3 --master_port=9993 train_stereo.py \
+--distributed \
+--launcher pytorch \
+--gpu_ids 0 1 2 \
+--name defomstereo_vits_instereo2k_mb2014_mb2021 \
+--batch_size 3 \
+--num_workers 6 \
+--train_datasets instereo2k middlebury_2014 middlebury_2021 \
+--train_folds 50 200 200 \
+--num_steps 100000 \
+--val_freq 200000 \
+--mixed_precision \
+--n_downsample 2 \
+--train_iters 18 \
+--scale_iters 8 \
+--idepth_scale 0.5 \
+--corr_levels 2 \
+--corr_radius 4 \
+--scale_list 0.125 0.25 0.5 0.75 1.0 1.25 1.5 2.0 \
+--scale_corr_radius 2 \
+--resume_ckpt checkpoints/defomstereo_vits_sceneflow.pth \
+> ${CHECKPOINT_DIR}/train.log 2>&1 &
